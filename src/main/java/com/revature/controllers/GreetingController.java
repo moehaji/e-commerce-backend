4b762760d1a6d35776b678e670cbab9762768c03
nabilgj <nabilgj.bnm@gmail.com>
2022-06-10 10:58:47 -0400

    implementing web socket

---
+++ src/main/java/com/revature/controllers/GreetingController.java
@@ -0,0 +1,33 @@
+package com.revature.controllers;
+
+import com.revature.messagingstompwebsocket.Greeting;
+import com.revature.messagingstompwebsocket.HelloMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.HtmlUtils;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+
+
+import javax.websocket.OnMessage;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topics/greetings")
+    @PostMapping
+    public Greeting greeting(@RequestBody HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+
+        System.out.println("coming from line 29 " + message );
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
